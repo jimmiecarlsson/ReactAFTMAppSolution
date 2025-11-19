e33604df37b2c7b2638178cf552d6f9ab031d158
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-11-19 17:11:29 +0100

    Add react-router-dom to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Card, Alert, Badge, Image } from "react-bootstrap"
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
+import { Container, Row, Col, Card, Alert, Badge, Image } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Link, Routes, Route } from "react-router-dom";
 
-import { FaGithub } from "react-icons/fa"
-import { FaQrcode } from "react-icons/fa"
+import { FaGithub, FaQrcode, FaInfoCircle } from "react-icons/fa";
 
-import AFTMSelect from "./components/AFTMSelect"
-import LengthSlider from "./components/LengthSlider"
+import AFTMSelect from "./components/AFTMSelect";
+import LengthSlider from "./components/LengthSlider";
 
-import { getIndicator } from "./utils/getIndicator"
-import { calcGramsPerMeter } from "./utils/calcPerMeter"
-import { calcHeadWeight } from "./utils/calcWeight"
-import { getAftmData } from "./utils/getAftmData"
-import imgQR from "./assets/qrcode.png"
+import { getIndicator } from "./utils/getIndicator";
+import { calcGramsPerMeter } from "./utils/calcPerMeter";
+import { calcHeadWeight } from "./utils/calcWeight";
+import { getAftmData } from "./utils/getAftmData";
 
+import imgQR from "./assets/qrcode.png";
 
-//triggar deploy
+import InfoPage from "./components/Pages/InfoPage.jsx";
+
 function App() {
 
     const [fullscreen, setFullscreen] = useState(true);
@@ -26,7 +27,6 @@ function App() {
         setFullscreen(breakpoint);
         setShow(true);
     }
-    
 
     const [aftm, setAftm] = useState("");
     const [length, setLength] = useState(9.14);
@@ -35,76 +35,110 @@ function App() {
     const weight = calcHeadWeight(aftm, length);
     const gramsPerMeter = calcGramsPerMeter(aftm);
 
-
     return (
-        <Container className="mt-5">
-            <Row className="justify-content-center">
-                <Col md={6} lg={4}>
-                    <Card className="shadow-sm">
-                        <Card.Body>
+        <Routes>
 
-                            <h2 className="text-center mb-4">AFTM Kalkylator</h2>
+            <Route
+                path="/"
+                element={
+                    <Container className="mt-5">
+                        <Row className="justify-content-center">
+                            <Col md={6} lg={4}>
+                                <Card className="shadow-sm">
+                                    <Card.Body>
 
-                            <AFTMSelect value={aftm} onChange={setAftm} />
+                                        <Link to="/InfoPage">
+                                            <FaInfoCircle />
+                                        </Link>
 
-                            {aftmInfo && (
-                                <>
-                                    <Alert variant="secondary">
-                                        Vikt för klassen: {gramsPerMeter} g/m
-                                    </Alert>
-                                    <Alert variant="secondary">
-                                        <strong>Vikter enligt AFTM:</strong><br />
-                                        Minimum: {aftmInfo.minWeight} g<br />
-                                        Standard: {aftmInfo.stdWeight} g<br />
-                                        Maximum: {aftmInfo.maxWeight} g
-                                    </Alert>
-                                </>
-                            )}                            
-                            <LengthSlider value={length} onChange={setLength} />
-                            {aftm && (
-                                <>
-                                    <div className="mt-0 mb-3">
-                                        Klumpens vikt: <strong>{weight} g</strong>
-                                    </div>
+                                        <h2 className="text-center mb-4">AFTM Kalkylator</h2>
 
-                                    {(() => {
-                                        const indicator = getIndicator(weight, aftmInfo);
-                                        return (
-                                            <div className="text-center mb-3 p-4">
-                                                <Badge bg={indicator.color} pill className="px-5 py-4 fs-5">
-                                                    {indicator.text}
-                                                </Badge>
-                                            </div>
-                                        );
-                                    })()}
-                                </>
-                            )}
-                            
+                                        <AFTMSelect value={aftm} onChange={setAftm} />
 
-                            <div className="text-center mt-5">
-                                <a href="https://jimmiecarlsson.github.io/" target="_blank" rel="noreferrer nofollow">Kodad av <FaGithub /></a>
-                            </div>
-                            <div className="text-left mt-2">
-                                <Card.Link onClick={() => handleShow(fullscreen)}><FaQrcode/></Card.Link>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Modal size="sm" show={show} onHide={() => setShow(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>AFTM</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Row>
-                        <Col>
-                            <Image src={imgQR} rounded fluid/>
-                        </Col>
-                    </Row>
-                    <a href="https://aftm-9d6548381914.herokuapp.com/" target="_blank" rel="noreferrer nofollow">https://aftm-9d6548381914.herokuapp.com/</a>
-                </Modal.Body>
-            </Modal>
-        </Container>
+                                        {aftmInfo && (
+                                            <>
+                                                <Alert variant="secondary">
+                                                    Vikt for klassen: {gramsPerMeter} g/m
+                                                </Alert>
+
+                                                <Alert variant="secondary">
+                                                    <strong>Vikter enligt AFTM:</strong><br />
+                                                    Minimum: {aftmInfo.minWeight} g<br />
+                                                    Standard: {aftmInfo.stdWeight} g<br />
+                                                    Maximum: {aftmInfo.maxWeight} g
+                                                </Alert>
+                                            </>
+                                        )}
+
+                                        <LengthSlider value={length} onChange={setLength} />
+
+                                        {aftm && (
+                                            <>
+                                                <div className="mt-0 mb-3">
+                                                    Klumpens vikt: <strong>{weight} g</strong>
+                                                </div>
+
+                                                {(() => {
+                                                    const indicator = getIndicator(weight, aftmInfo);
+                                                    return (
+                                                        <div className="text-center mb-3 p-4">
+                                                            <Badge bg={indicator.color} pill className="px-5 py-4 fs-5">
+                                                                {indicator.text}
+                                                            </Badge>
+                                                        </div>
+                                                    );
+                                                })()}
+                                            </>
+                                        )}
+
+                                        <div className="text-center mt-5">
+                                            <a
+                                                href="https://jimmiecarlsson.github.io/"
+                                                target="_blank"
+                                                rel="noreferrer nofollow"
+                                            >
+                                                Kodad av <FaGithub />
+                                            </a>
+                                        </div>
+
+                                        <div className="text-left mt-2">
+                                            <Card.Link onClick={() => handleShow(fullscreen)}>
+                                                <FaQrcode />
+                                            </Card.Link>
+                                        </div>
+
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+
+                        <Modal size="sm" show={show} onHide={() => setShow(false)}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>AFTM</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Row>
+                                    <Col>
+                                        <Image src={imgQR} rounded fluid />
+                                    </Col>
+                                </Row>
+                                <a
+                                    href="https://aftm-9d6548381914.herokuapp.com/"
+                                    target="_blank"
+                                    rel="noreferrer nofollow"
+                                >
+                                    https://aftm-9d6548381914.herokuapp.com/
+                                </a>
+                            </Modal.Body>
+                        </Modal>
+
+                    </Container>
+                }
+            />
+
+            <Route path="/InfoPage" element={<InfoPage />} />
+
+        </Routes>
     );
 }
 
---
+++ src/components/Pages/InfoPage.jsx
@@ -0,0 +1,29 @@
+﻿
+import React from 'react'
+import { Container, Row, Col, Card, Alert, Badge, Image } from "react-bootstrap"
+import { Link } from "react-router-dom";
+
+const InfoPage = () => {
+
+
+
+
+    return (
+        <>
+            <Container className="mt-5">
+                <Row>
+                    <Col>
+                        <div className="text-white">
+                            Mer info kommer här sen.
+                        </div>  
+                        <Link to="/">
+                            Tillbaka
+                        </Link>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+};
+
+export default InfoPage;
